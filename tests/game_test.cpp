9879648004f2d661479864f284803cc219b955b6
Wmanfrompoland <zroyt9898@gmail.com>
2025-05-18 11:24:20 +0200

    add tests for #72

--- tests/game_test.cpp
+++ tests/game_test.cpp
@@ -22,3 +22,16 @@ TEST_CASE("Moving 0 cards to the foundation(invalid)"){
     game.tableau[0][0].hide();
     REQUIRE(game.processInput("k4:sk") != ""); //If command is executed properly, it will return an empty string. In this case the command shouldn't be executed, so it should return a non-empty string
 }
+
+TEST_CASE("Moving black cards onto black cards(invalid)"){
+    Game game(true);
+    game.tableau = {{Card(2,0),Card(1, 1)}, {Card(3,0)}, {}, {}, {}, {}, {}};
+    REQUIRE(game.processInput("k1:k2") != ""); //If command is executed properly, it will return an empty string. In this case the command shouldn't be executed, so it should return a non-empty string
+}
+
+TEST_CASE("Moving red cards onto red cards(invalid)"){
+    Game game(true);
+    game.tableau = {{Card(2,1),Card(1, 0)}, {Card(3,1)}, {}, {}, {}, {}, {}};
+    game.tableau[0][0].hide();
+    REQUIRE(game.processInput("k1:k2") != ""); //If command is executed properly, it will return an empty string. In this case the command shouldn't be executed, so it should return a non-empty string
+}
